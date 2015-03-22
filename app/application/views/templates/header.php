59a1ff9f36a8f8f18f96171ecbb0a4f0b5204f22
karl-puusaar <karl-p@outlook.com>
2015-03-22 22:59:29 +0200

    Test

--- app/application/views/templates/header.php
+++ app/application/views/templates/header.php
@@ -1,9 +1,10 @@
 <!DOCTYPE html>
-<script type="text/javascript" src="https://cdnjs.cloudflare.com/ajax/libs/html5shiv/3.7.2/html5shiv.min.js"></script>
+
 <html>
 
 <head>
 	<meta charset="utf-8">
+	<script type="text/javascript" src="https://cdnjs.cloudflare.com/ajax/libs/html5shiv/3.7.2/html5shiv.min.js"></script>
 	<title><?php echo $title ?> - Postimees 2</title>
 </head>
 <body>
