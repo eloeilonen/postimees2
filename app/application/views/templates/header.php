6fbb6aa314dcd70eee89b69f153d9a0a6891a1f5
karl-puusaar <karl-p@outlook.com>
2015-03-22 23:04:28 +0200

    Kustutasin rea, mis ei teinud mitte midagi

--- app/application/views/templates/header.php
+++ app/application/views/templates/header.php
@@ -1,10 +1,7 @@
 <!DOCTYPE html>
-
 <html>
-
 <head>
 	<meta charset="utf-8">
-	<script type="text/javascript" src="https://cdnjs.cloudflare.com/ajax/libs/html5shiv/3.7.2/html5shiv.min.js"></script>
 	<title><?php echo $title ?> - Postimees 2</title>
 </head>
 <body>
