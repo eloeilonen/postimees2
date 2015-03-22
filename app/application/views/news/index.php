3eebe0da4db1f3ec1053b8e4bc6862c2e497e047
karl-puusaar <karl-p@outlook.com>
2015-03-22 20:17:02 +0200

    Asendasin </br> <br>-ga

--- app/application/views/news/index.php
+++ app/application/views/news/index.php
@@ -1,9 +1,9 @@
-<?php echo '<p><a href="editor">Logi sisse</a></p><hr></br>' ?>
+<?php echo '<p><a href="editor">Logi sisse</a></p><hr><br>' ?>
 <?php foreach ($uudis as $news_item): ?>
 
     <p><a href="news/<?php echo $news_item['uudise_ID'] ?>"><h2><?php echo $news_item['uudise_PEALKIRI'] ?></h2></a></p>
     <div class="main">
-		<a href="news/<?php echo $news_item['uudise_ID'] ?>"><?php echo $news_item['uudise_PILT'] ?></a></br>
+		<a href="news/<?php echo $news_item['uudise_ID'] ?>"><?php echo $news_item['uudise_PILT'] ?></a><br>
         <?php echo $news_item['uudise_EELVAADE']?>
     </div>
 
@@ -13,7 +13,7 @@
 
 <?php foreach ($news_stats as $news_stats): ?>
 	<a href="news/<?php echo $news_stats['uudise_ID'] ?>"><?php echo $news_stats['uudise_PEALKIRI']?></a>
-	<?php echo ' | '.$news_stats['count'].'</br>'?>
+	<?php echo ' | '.$news_stats['count'].'<br>'?>
 <?php endforeach ?>
 
-<?php echo '</br><hr><p></p><a href="editor/create">Lisa uudis</a></p>' ?>
+<?php echo '<br><hr><p></p><a href="editor/create">Lisa uudis</a></p>' ?>
--- app/application/views/templates/footer.php
+++ app/application/views/templates/footer.php
@@ -1,4 +1,4 @@
-</br><hr>
+<br><hr>
 <strong>Veebirakenduste loomine Kevad 2015 | Meeskond - Postimees2</strong>
 </body>
 </html>
