3cff5bd32fa57433bfc836c8c766c828bbb6ae18
karl-puusaar <karl-p@outlook.com>
2015-03-22 22:53:04 +0200

    Lisasin ID väärtused

--- app/application/views/editor/create.php
+++ app/application/views/editor/create.php
@@ -4,29 +4,29 @@
 
 <?php echo form_open('editor/create') ?>
 
-	<label for="pealkiri-field">Pealkiri</label>
-	<input type="text" name="pealkiri" id="pealkiri-field"/><br />
+	<label for="pealkiri">Pealkiri</label>
+	<input type="text" name="pealkiri" id="pealkiri"/><br />
 
 	<label for="teema">Teema</label>
-	<input type="text" name="teema" value="2"/><br />
+	<input type="text" name="teema" id="teema" value="2"/><br />
 
 	<p>Asukoha andmed</p>
 	<label for="lat_coord">Asukoha pikkuskraad</label>
-	<input type="number" name="lat_coord" value="58.378355"/>
+	<input type="number" name="lat_coord" id="lat_coord" value="58.378355"/>
 	<label for="lon_coord">Asukoha laiuskraad</label>
-	<input type="number" name="lon_coord" value="26.714663"/><br />
+	<input type="number" name="lon_coord" id="lon coord" value="26.714663"/><br />
 
 	<label for="tekst">Tekst</label>
-	<textarea name="tekst"></textarea><br />
+	<textarea name="tekst" id="tekst"></textarea><br />
 
 	<label for="eelvaade">Eelvaade</label>
-	<textarea name="eelvaade"></textarea><br />
+	<textarea name="eelvaade" id="eelvaade"></textarea><br />
 
 	<label for="date">Kuupäev</label>
-	<input type="date" name="date" value="2015-01-01"/><br />
+	<input type="polyfill" name="date" id="date" value="2015-01-01"/><br />
 
 	<label for="autor">Autor</label>
-	<input type="number" name="autor" value="1"/><br />
+	<input type="number" name="autor" id="autor" value="1"/><br />
 
 	<input type="submit" name="submit" value="Salvesta" />
 
