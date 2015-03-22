3e32561ec2f0507394b8657780afd96855a9e123
karl-puusaar <karl-p@outlook.com>
2015-03-22 22:12:55 +0200

    Test

--- app/application/views/editor/create.php
+++ app/application/views/editor/create.php
@@ -4,28 +4,28 @@
 
 <?php echo form_open('editor/create') ?>
 
-	<label for="pealkiri">Pealkiri</label>
+	<label for="pealkiri-id">Pealkiri</label>
 	<input type="text" name="pealkiri" /><br />
 
-	<label for="teema">Teema</label>
+	<label for="teema-id">Teema</label>
 	<input type="text" name="teema" value="2"/><br />
 
 	<p>Asukoha andmed</p>
-	<label for="lat_coord">Asukoha pikkuskraad</label>
+	<label for="lat_coord-id">Asukoha pikkuskraad</label>
 	<input type="number" name="lat_coord" value="58.378355"/>
-	<label for="lon_coord">Asukoha laiuskraad</label>
+	<label for="lon_coord-id">Asukoha laiuskraad</label>
 	<input type="number" name="lon_coord" value="26.714663"/><br />
 
-	<label for="tekst">Tekst</label>
+	<label for="tekst-id">Tekst</label>
 	<textarea name="tekst"></textarea><br />
 
-	<label for="eelvaade">Eelvaade</label>
+	<label for="eelvaade-id">Eelvaade</label>
 	<textarea name="eelvaade"></textarea><br />
 
-	<label for="date">Kuupäev</label>
+	<label for="date">Kuupäev-id</label>
 	<input type="date" name="date" value="2015-01-01"/><br />
 
-	<label for="autor">Autor</label>
+	<label for="autor">Autor-id</label>
 	<input type="number" name="autor" value="1"/><br />
 
 	<input type="submit" name="submit" value="Salvesta" />
