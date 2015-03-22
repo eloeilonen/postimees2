db1821b2c883a72f54b179170c11d09286708fdd
karl-puusaar <karl-p@outlook.com>
2015-03-22 20:05:01 +0200

    Test

--- app/application/views/news/view.php
+++ app/application/views/news/view.php
@@ -1,10 +1,10 @@
 <?php
 echo '<h2>'.$news_item['uudise_PEALKIRI'].'</h2>';
-echo $news_item['uudise_TEKST'].'</br>';
-echo 'Autor: '.$news_author['kasutaja_EESNIMI'].' '.$news_author['kasutaja_PERENIMI'].' '.$news_author['kasutaja_EMAIL'].'</br>';
-//echo '<input type="button" name="Edit" value="Muuda"/></br>';
-//echo '<input type="submit" name="Delete" value="Kustuta"</br>';
+echo $news_item['uudise_TEKST'].'<br>';
+echo 'Autor: '.$news_author['kasutaja_EESNIMI'].' '.$news_author['kasutaja_PERENIMI'].' '.$news_author['kasutaja_EMAIL'].'<br>';
+//echo '<input type="button" name="Edit" value="Muuda"/><br>';
+//echo '<input type="submit" name="Delete" value="Kustuta"<br>';
 
 $url = htmlspecialchars($_SERVER['HTTP_REFERER']);
-echo "<a href='$url'>Kõik uudised</a></br>";
+echo "<a href='$url'>Kõik uudised</a><br>";
 
