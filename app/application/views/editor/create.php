28dbef75122f2ea3a2a085333224d581de38ed50
karl-puusaar <karl-p@outlook.com>
2015-03-22 21:54:47 +0200

    Lisasin õiged andme tüübid

--- app/application/views/editor/create.php
+++ app/application/views/editor/create.php
@@ -5,16 +5,16 @@
 <?php echo form_open('editor/create') ?>
 
 	<label for="pealkiri">Pealkiri</label>
-	<input type="input" name="pealkiri" /><br />
+	<input type="text" name="pealkiri" /><br />
 
 	<label for="teema">Teema</label>
-	<input type="input" name="teema" value="2"/><br />
+	<input type="text" name="teema" value="2"/><br />
 
 	<p>Asukoha andmed</p>
 	<label for="lat_coord">Asukoha pikkuskraad</label>
-	<input type="input" name="lat_coord" value="58.378355"/>
+	<input type="number" name="lat_coord" value="58.378355"/>
 	<label for="lon_coord">Asukoha laiuskraad</label>
-	<input type="input" name="lon_coord" value="26.714663"/><br />
+	<input type="number" name="lon_coord" value="26.714663"/><br />
 
 	<label for="tekst">Tekst</label>
 	<textarea name="tekst"></textarea><br />
@@ -23,10 +23,10 @@
 	<textarea name="eelvaade"></textarea><br />
 
 	<label for="date">Kuupäev</label>
-	<input type="input" name="date" value="2015-01-01"/><br />
+	<input type="date" name="date" value="2015-01-01"/><br />
 
 	<label for="autor">Autor</label>
-	<input type="input" name="autor" value="1"/><br />
+	<input type="number" name="autor" value="1"/><br />
 
 	<input type="submit" name="submit" value="Salvesta" />
 
@@ -34,5 +34,5 @@
 
 	<?php
 	  $url = htmlspecialchars($_SERVER['HTTP_REFERER']);
-	  echo "<a href='$url'>Katkesta</a></br>";
+	  echo "<a href='$url'>Katkesta</a><br>";
 	?>
