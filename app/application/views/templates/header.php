a8c2305e43d7e95d657938d47ead672521c4928e
karl-puusaar <karl-p@outlook.com>
2015-04-05 18:48:26 +0300

    Google login

--- app/application/views/templates/header.php
+++ app/application/views/templates/header.php
@@ -1,8 +1,25 @@
 <!DOCTYPE html>
+
+<h1>Postimees 2</h1>
+
 <html>
 <head>
+	
 	<meta charset="utf-8">
+
 	<title><?php echo $title ?> - Postimees 2</title>
+	
+	<script src="https://apis.google.com/js/client:platform.js" async defer></script>
+	<span id="signinButton">
+  		<span
+		    class="g-signin"
+		    data-callback="signinCallback"
+		    data-clientid="1082678952766-30kadv0uf0didhdidpefd66um24gom78.apps.googleusercontent.com"
+		    data-cookiepolicy="single_host_origin"
+		    data-requestvisibleactions="http://schema.org/AddAction"
+		    data-scope="https://www.googleapis.com/auth/plus.login">
+		</span>
+	</span>
+	
 </head>
 <body>
-	<h1>Postimees 2</h1>
