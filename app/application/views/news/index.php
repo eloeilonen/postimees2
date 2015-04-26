64e6bfeba3f53238a3ab3494f2e59dbaf9f58f9a
karl-puusaar <karl-p@outlook.com>
2015-04-27 00:05:10 +0300

    </a> oli puudu, vist

--- app/application/views/news/index.php
+++ app/application/views/news/index.php
@@ -11,7 +11,7 @@
 			<h2 class="blog-post-title"><a href="news/<?php echo $news_item['uudise_ID'] ?>"><?php echo $news_item['uudise_PEALKIRI'] ?></a></h2>
 			<div class="row">
 				<div class="col-sm-4">
-					<a href="news/<?php echo $news_item['uudise_ID'] ?>"><img src="<?php echo $news_item['uudise_PILT'] ?>" alt="Uudise pilt" class="img-thumbnail" style="min-height:200px;height:200px;">
+					<a href="news/<?php echo $news_item['uudise_ID'] ?>"><img src="<?php echo $news_item['uudise_PILT'] ?>" alt="Uudise pilt" class="img-thumbnail" style="min-height:200px;height:200px;"></a>
 				</div>
 			<p class="text-justify"><?php echo $news_item['uudise_EELVAADE']?></p>
 			</div>
