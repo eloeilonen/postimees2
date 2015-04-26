aebe001ef64879641b28007728d415a4837b680a
karl-puusaar <karl-p@outlook.com>
2015-04-26 23:46:31 +0300

    Sisselogimise lehe parandused 2

--- app/application/views/login_view.php
+++ app/application/views/login_view.php
@@ -1,4 +1,4 @@
-<meta name="viewport" content="width=device-width, initial-scale=1.0">
+<meta name="viewport" content="width=device-width, initial-scale=1.0"/>
 <link href="<?php echo base_url('/assets/css/bootstrap.min.css'); ?>" rel="stylesheet" type="text/css" />
 
      <style type="text/css">
