008cb9e927ed235782bdb091ff64637c49deca09
karl-puusaar <karl-p@outlook.com>
2015-04-26 23:42:29 +0300

    Uudise lehekülje parandused

--- app/application/views/news/view.php
+++ app/application/views/news/view.php
@@ -1,12 +1,10 @@
-</head><body>
-
 <p><a href="<?php echo base_url()?>login">Logi sisse</a></p><hr><br>
 
 <div class="row">
 	<div class="col-sm-8 blog-main">
 		<div class="blog-post">
 			<h2 class="blog-post-title"><?php echo $news_item['uudise_PEALKIRI'] ?></h2>
-			<img src="<?php echo $news_item['uudise_PILT'] ?>" alt="Uudise pilt" class="img-thumbnail" style="min-height:500px;height:500px;"></a>
+			<img src="<?php echo $news_item['uudise_PILT'] ?>" alt="Uudise pilt" class="img-thumbnail" style="min-height:500px;height:500px;">
 			<br><br><p class="text-justify"><?php echo $news_item['uudise_TEKST'] ?></p>
 			<hr>
 			<p class="blog-post-meta"><?php echo $news_author['kasutaja_EESNIMI'].' '.$news_author['kasutaja_PERENIMI'].' '.$news_author['kasutaja_EMAIL'] ?></p>
