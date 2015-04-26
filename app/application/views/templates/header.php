a1414cca475a21cfced28bb3992af34736346a42
karl-puusaar <karl-p@outlook.com>
2015-04-26 22:35:00 +0300

    Test

--- app/application/views/templates/header.php
+++ app/application/views/templates/header.php
@@ -1,4 +1,21 @@
 <!DOCTYPE html>
+<script>
+  window.fbAsyncInit = function() {
+    FB.init({
+      appId      : '418927931620571',
+      xfbml      : true,
+      version    : 'v2.3'
+    });
+  };
+
+  (function(d, s, id){
+     var js, fjs = d.getElementsByTagName(s)[0];
+     if (d.getElementById(id)) {return;}
+     js = d.createElement(s); js.id = id;
+     js.src = "//connect.facebook.net/en_US/sdk.js";
+     fjs.parentNode.insertBefore(js, fjs);
+   }(document, 'script', 'facebook-jssdk'));
+</script>
 <html>
 <head>
 	<meta charset="utf-8">
