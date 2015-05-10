02301d0bdbdb4c9eb4a9a7905c4cf90b102b7eb6
eloeilonen <elo.eilonen@gmail.com>
2015-05-11 00:04:32 +0300

    logimise kamm ikka

--- app/application/controllers/base_controller.php
+++ app/application/controllers/base_controller.php
@@ -11,18 +11,19 @@ class base_controller extends CI_Controller
             '808877592535768',
             // Elo
             '10153304215197430',
+            '1385762988420752',
         );
         parent::__construct();
         $this->load->database();
         $this->load->library('session');
         // dev
-        //$this->load->library('facebook', array('appId' => '420031188176912', 'secret' => 'c064fa8d54d8518d040874c4384caf2e'));
+        $this->load->library('facebook', array('appId' => '420031188176912', 'secret' => 'c064fa8d54d8518d040874c4384caf2e'));
         // test
-        $this->load->library('facebook', array('appId' => '418927931620571', 'secret' => '1cdb542937f1e2ea48fcb9955b7540b5'));
+        //$this->load->library('facebook', array('appId' => '418927931620571', 'secret' => '1cdb542937f1e2ea48fcb9955b7540b5'));
 
         // If user is set then get profile info
-        $this->fb_user = $this->facebook->getUser() ? $this->facebook->api('/me/') : false;
-        var_export($this->fb_user);
+        $this->fb_user = $this->facebook->getUser() ? $<this->facebook->api('/me/') : false;
+        //var_export($this->fb_user);
         $this->isAuthenticated = (array_search($this->fb_user['id'], $this->authenticatedUsers) !== false) ? 'true' : false;
     }
 
