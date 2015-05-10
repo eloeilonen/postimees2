85344a86c2b73fe15764ee9330f4d6cdd65a33fc
eloeilonen <elo.eilonen@gmail.com>
2015-05-10 22:39:02 +0300

    merge issues resolved

--- app/application/views/news/view.php
+++ app/application/views/news/view.php
@@ -5,14 +5,9 @@
 			<img src="<?php echo $news_item['uudise_PILT'] ?>" alt="Uudise pilt" class="img-thumbnail" style="min-height:500px;height:500px;">
 			<br><br><p class="text-justify"><?php echo $news_item['uudise_TEKST'] ?></p>
 			<hr>
-<<<<<<< HEAD
 			<p class="blog-post-meta"><?php echo $news_author['kasutaja_EESNIMI'].' '.$news_author['kasutaja_PERENIMI'].' ' ?> <a href="mailto:"<?php echo $news_author['kasutaja_EMAIL'] ?>><?php echo $news_author['kasutaja_EMAIL'] ?></a></p>
 			<p class="blog-post-meta"><p class="text-right"><?php echo $news_item['uudise_KUUPAEV']?></p></p>
 			<p class="blog-post-meta"><a href="<?php echo base_url()?>editor/update/<?php echo $news_item['uudise_ID']?>">Muuda</a></p><hr><br>
-=======
-			<p class="blog-post-meta"><?php echo $news_author['kasutaja_EESNIMI'].' '.$news_author['kasutaja_PERENIMI'].' '.$news_author['kasutaja_EMAIL'] ?></p>
-			<p class="blog-post-meta"><p class="text-right"><?php echo $news_item['uudise_KUUPAEV']?></p>
->>>>>>> 359013f39f7a6084d8b7d96d80fc6a98c0c771c4
 		</div>
 	</div>
 
--- app/application/views/templates/header.php
+++ app/application/views/templates/header.php
@@ -1,31 +1,6 @@
 <!DOCTYPE html>
 <html>
 <head>
-<<<<<<< HEAD
-	<meta charset="utf-8">
-	<title><?php echo $title ?> - Postimees 2</title>
-	<link rel="stylesheet" href="<?php echo base_url("assets/css/bootstrap.css"); ?>" />
-	<script type="text/javascript" src="<?php echo base_url("assets/js/jquery-1.11.2.js"); ?>"></script>
-	<script type="text/javascript" src="<?php echo base_url("assets/js/bootstrap.js"); ?>"></script>
-	<div id="postimees2" class="container">
-	<div class="jumbotron" style="background-color:lavender;">
-		<h1><a href="<?php echo base_url(); ?>">Postimees 2</a></h1>
-		<p>Veebirakenduste loomine 2014/15 kevad</p>
-	</div>
-<!--
-	<script src="https://apis.google.com/js/client:platform.js" async defer></script>
-	<span id="signinButton">
-  		<span
-		    class="g-signin"
-		    data-callback="signinCallback"
-		    data-clientid="1082678952766-30kadv0uf0didhdidpefd66um24gom78.apps.googleusercontent.com"
-		    data-cookiepolicy="single_host_origin"
-		    data-requestvisibleactions="http://schema.org/AddAction"
-		    data-scope="https://www.googleapis.com/auth/userinfo.email">
-		</span>
-	</span>
--->
-=======
     <link href="<?php echo base_url('/assets/css/bootstrap.min.css'); ?>" rel="stylesheet" type="text/css"/>
     <meta charset="utf-8">
     <title><?php echo $title ?> - Postimees 2</title>
@@ -98,19 +73,4 @@
 
         <p>Veebirakenduste loomine 2014/15 kevad</p>
     </div>
-
-    <!--
-        <script src="https://apis.google.com/js/client:platform.js" async defer></script>
-        <span id="signinButton">
-              <span
-                class="g-signin"
-                data-callback="signinCallback"
-                data-clientid="1082678952766-30kadv0uf0didhdidpefd66um24gom78.apps.googleusercontent.com"
-                data-cookiepolicy="single_host_origin"
-                data-requestvisibleactions="http://schema.org/AddAction"
-                data-scope="https://www.googleapis.com/auth/userinfo.email">
-            </span>
-        </span>
-    -->
->>>>>>> 359013f39f7a6084d8b7d96d80fc6a98c0c771c4
 
