5c1387ffbe1dc022cd67645b20cebfbac172d72b
eloeilonen <elo.eilonen@gmail.com>
2015-05-11 00:07:32 +0300

    logimise kamm ikka

--- app/application/controllers/base_controller.php
+++ app/application/controllers/base_controller.php
@@ -11,6 +11,7 @@ class base_controller extends CI_Controller
             '808877592535768',
             // Elo
             '10153304215197430',
+            // Test
             '1385762988420752',
         );
         parent::__construct();
@@ -22,7 +23,7 @@ class base_controller extends CI_Controller
         //$this->load->library('facebook', array('appId' => '418927931620571', 'secret' => '1cdb542937f1e2ea48fcb9955b7540b5'));
 
         // If user is set then get profile info
-        $this->fb_user = $this->facebook->getUser() ? $<this->facebook->api('/me/') : false;
+        $this->fb_user = $this->facebook->getUser() ? $this->facebook->api('/me/') : false;
         //var_export($this->fb_user);
         $this->isAuthenticated = (array_search($this->fb_user['id'], $this->authenticatedUsers) !== false) ? 'true' : false;
     }
