6b0d08d79b4aefd318264ed0bd4fa102b846a8b1
karl-puusaar <karl-p@outlook.com>
2015-05-10 19:41:04 +0300

    FB logimine - you have no power here

--- app/application/controllers/base_controller.php
+++ app/application/controllers/base_controller.php
@@ -2,14 +2,21 @@
 
 class base_controller extends CI_Controller {
 
+    protected $authenticatedUsers;
+
     public function __construct() {
+        $this->authenticatedUsers = array(
+            // Brandon
+            '808877592535768',
+        );
         parent::__construct();
         $this->load->database();
         $this->load->library('session');
-        $this->load->library('facebook', array('appId' => '418927931620571', 'secret' => '1cdb542937f1e2ea48fcb9955b7540b5'));
+        $this->load->library('facebook', array('appId' => '420031188176912', 'secret' => 'c064fa8d54d8518d040874c4384caf2e'));
 
         // If user is set then get profile info
         $this->fb_user = $this->facebook->getUser() ? $this->facebook->api('/me/') : false;
+        $this->isAuthenticated = (array_search($this->fb_user['id'], $this->authenticatedUsers) !== false) ? 'true' : false;
     }
 
 }
--- app/application/views/templates/header.php
+++ app/application/views/templates/header.php
@@ -1,15 +1,15 @@
 <!DOCTYPE html>
 <html>
 <head>
-<link href="<?php echo base_url('/assets/css/bootstrap.min.css'); ?>" rel="stylesheet" type="text/css" />
-	<meta charset="utf-8">
-	<title><?php echo $title ?> - Postimees 2</title>
-	<link rel="stylesheet" href="<?php echo base_url("assets/css/bootstrap.css"); ?>" />
-	<link rel="stylesheet" href="//maxcdn.bootstrapcdn.com/font-awesome/4.3.0/css/font-awesome.min.css" />
-	<script type="text/javascript" src="<?php echo base_url("assets/js/jquery-1.11.2.js"); ?>"></script>
-	<script type="text/javascript" src="<?php echo base_url("assets/js/bootstrap.js"); ?>"></script>
+    <link href="<?php echo base_url('/assets/css/bootstrap.min.css'); ?>" rel="stylesheet" type="text/css"/>
+    <meta charset="utf-8">
+    <title><?php echo $title ?> - Postimees 2</title>
+    <link rel="stylesheet" href="<?php echo base_url("assets/css/bootstrap.css"); ?>"/>
+    <link rel="stylesheet" href="//maxcdn.bootstrapcdn.com/font-awesome/4.3.0/css/font-awesome.min.css"/>
+    <script type="text/javascript" src="<?php echo base_url("assets/js/jquery-1.11.2.js"); ?>"></script>
+    <script type="text/javascript" src="<?php echo base_url("assets/js/bootstrap.js"); ?>"></script>
     <META HTTP-EQUIV="cache-control" CONTENT="public">
-    <meta http-equiv="expires" content="<?= gmdate('D, d M Y H:i:s', time() + (60*60*24*45)) . ' GMT' ?> GMT">
+    <meta http-equiv="expires" content="<?= gmdate('D, d M Y H:i:s', time() + (60 * 60 * 24 * 45)) . ' GMT' ?> GMT">
 </head>
 <body>
 
@@ -29,49 +29,68 @@
         <!-- Collect the nav links, forms, and other content for toggling -->
         <div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
             <ul class="nav navbar-nav navbar-right">
-                <?php if(! empty($this->fb_user['id'])): ?>
-                <li class="dropdown">
-                    <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-expanded="false">
-                        <img src="https://graph.facebook.com/<?= $this->fb_user['id'] ?>/picture" height="20px" width="20px">
-                        <?=  $this->fb_user['name'] ?><span class="caret"></span></a>
-                    <ul class="dropdown-menu" role="menu">
-                        <li style="width: 300px">
-                            <img class="pull-left thumbnail" style="margin: 10px" src="https://graph.facebook.com/<?= $this->fb_user['id'] ?>/picture?type=large" height="100px" width="100px">
-                            <ul class="pull-left nav" style="margin: 10px">
-                                <li><?= $this->fb_user['name'] ?></li>
-                                <li><?= $this->fb_user['email'] ?></li>
-                                <li class="divider"></li>
-                                <li><a href="<?= base_url('fb_login/logout') ?>">logi v√§lja</a></li>
-                            </ul>
-                        </li>
-                    </ul>
-                </li>
+                <?php if (!empty($this->fb_user['id'])): ?>
+                    <li class="dropdown">
+                        <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-expanded="false">
+                            <img src="https://graph.facebook.com/<?= $this->fb_user['id'] ?>/picture" height="20px" width="20px">
+                            <?= $this->fb_user['name'] ?><span class="caret"></span></a>
+                        <ul class="dropdown-menu" role="menu">
+                            <li style="width: 300px">
+                                <img class="pull-left thumbnail" style="margin: 10px" src="https://graph.facebook.com/<?= $this->fb_user['id'] ?>/picture?type=large" height="100px" width="100px">
+                                <ul class="pull-left nav" style="margin: 10px">
+                                    <li><?= $this->fb_user['name'] ?></li>
+                                    <li><?= $this->fb_user['email'] ?></li>
+                                    <li class="divider"></li>
+                                    <li><a href="<?= base_url('fb_login/logout') ?>">logi v‰lja</a></li>
+                                </ul>
+                            </li>
+                        </ul>
+                    </li>
                 <?php else: ?>
-                <li><a href="<?= base_url('fb_login') ?>"><i class="fa fa-facebook"></i> logi sisse</a></li>
+                    <li><a href="<?= base_url('fb_login') ?>"><i class="fa fa-facebook"></i> logi sisse</a></li>
                 <?php endif; ?>
             </ul>
-        </div><!-- /.navbar-collapse -->
-    </div><!-- /.container-fluid -->
+        </div>
+        <!-- /.navbar-collapse -->
+    </div>
+    <!-- /.container-fluid -->
 </nav>
 
+<?php
 
-	<div id="postimees2" class="container">
-	<div class="jumbotron" style="background-color:lavender;">
-		<h1><a href="<?php echo base_url('/news'); ?>">Postimees 2</a></h1>
-		<p>Veebirakenduste loomine 2014/15 kevad</p>
-	</div>
+if(! empty($this->fb_user['id']) AND $this->isAuthenticated == false) {
+    echo '<center><img src="http://i.imgur.com/pZ96Rxa.png" /></center>';
+    die();
+}
 
-<!--
-	<script src="https://apis.google.com/js/client:platform.js" async defer></script>
-	<span id="signinButton">
-  		<span
-		    class="g-signin"
-		    data-callback="signinCallback"
-		    data-clientid="1082678952766-30kadv0uf0didhdidpefd66um24gom78.apps.googleusercontent.com"
-		    data-cookiepolicy="single_host_origin"
-		    data-requestvisibleactions="http://schema.org/AddAction"
-		    data-scope="https://www.googleapis.com/auth/userinfo.email">
-		</span>
-	</span>
--->
+?>
+
+<?php if (isset($_SESSION['messages'])): ?>
+    <div class="alert alert-danger">
+        <?php foreach($_SESSION['messages'] as $msg): ?>
+            <?= $msg ?><br />
+        <?php endforeach; ?>
+    </div>
+<?php endif; ?>
+
+<div id="postimees2" class="container">
+    <div class="jumbotron" style="background-color:lavender;">
+        <h1><a href="<?php echo base_url('/news'); ?>">Postimees 2</a></h1>
+
+        <p>Veebirakenduste loomine 2014/15 kevad</p>
+    </div>
+
+    <!--
+        <script src="https://apis.google.com/js/client:platform.js" async defer></script>
+        <span id="signinButton">
+              <span
+                class="g-signin"
+                data-callback="signinCallback"
+                data-clientid="1082678952766-30kadv0uf0didhdidpefd66um24gom78.apps.googleusercontent.com"
+                data-cookiepolicy="single_host_origin"
+                data-requestvisibleactions="http://schema.org/AddAction"
+                data-scope="https://www.googleapis.com/auth/userinfo.email">
+            </span>
+        </span>
+    -->
 
