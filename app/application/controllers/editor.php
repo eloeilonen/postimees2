b72a8443b8f9e41e26e066413243c030ebe08d0a
Brandon20 <brandon1@hot.ee>
2015-05-10 13:41:28 +0300

    FB login

--- app/application/controllers/editor.php
+++ app/application/controllers/editor.php
@@ -1,11 +1,11 @@
 <?php
-class Editor extends CI_Controller {
+
+require_once 'base_controller.php';
+class Editor extends base_controller {
 
 	public function __construct()
 	{
 		parent::__construct();
-		$this->load->database();
-		$this->load->library('session');
 		$this->load->model('editor_model');
 	}
 
