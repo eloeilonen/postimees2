827477d7803c8a806ded85a21724c2091a0ade19
eloeilonen <elo.eilonen@gmail.com>
2015-05-10 22:40:31 +0300

    base_conroller tagasi

--- app/application/controllers/base_controller.php
+++ app/application/controllers/base_controller.php
@@ -13,7 +13,7 @@ class base_controller extends CI_Controller
         parent::__construct();
         $this->load->database();
         $this->load->library('session');
-        $this->load->library('facebook', array('appId' => '420031188176912', 'secret' => 'c064fa8d54d8518d040874c4384caf2e'));
+        $this->load->library('facebook', array('appId' => '418927931620571', 'secret' => '1cdb542937f1e2ea48fcb9955b7540b5'));
 
         // If user is set then get profile info
         $this->fb_user = $this->facebook->getUser() ? $this->facebook->api('/me/') : false;
