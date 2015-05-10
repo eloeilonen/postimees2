34152805863c84420f939e4cae1ed43efea6c7df
karl-puusaar <karl-p@outlook.com>
2015-05-10 18:30:07 +0300

    Tõstsin FB logimise õigesse kausta

--- app/application/controllers/login.php
+++ app/application/controllers/login.php
@@ -1,12 +1,12 @@
 <?php if ( ! defined('BASEPATH')) exit('No direct script access allowed');
 
-class login extends CI_Controller
+require_once 'base_controller.php';
+class login extends base_controller
 {
 
      public function __construct()
      {
           parent::__construct();
-          $this->load->library('session');
           $this->load->helper('form');
           $this->load->helper('url');
           $this->load->helper('html');
